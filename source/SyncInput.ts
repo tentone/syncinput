a343dc36f61fe710a39f9ccec15001a22d42aa7d
Tentone <tentone@outlook.com>
2023-01-21 19:05:20 +0000

    TS

--- source/SyncInput.ts
+++
@@ -1,8 +0,0 @@
-var VERSION = "V1.2";
-
-export {VERSION}
-export {Gamepad} from "./Gamepad.js";
-export {Key} from "./Key.js";
-export {Keyboard} from "./Keyboard.js";
-export {Mouse} from "./Mouse.js";
-export {Vector2} from "./Vector2.js";
---
+++ source/main.ts
@@ -0,0 +1,9 @@
+var VERSION = "V1.2";
+
+export {VERSION}
+
+export {Gamepad} from "./gamepad.ts";
+export {Key} from "./key.ts";
+export {Keyboard} from "./keyboard.ts";
+export {Mouse} from "./mouse.ts";
+export {Vector2} from "./vector2.ts";
